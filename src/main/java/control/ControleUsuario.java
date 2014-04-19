4fccbb0fca8f5a652626936f2a1b94da973a8f4c
felipe.muner <felipe.muner@gmail.com>
2014-04-19 08:18:49 -0300

    subindo

--- src/main/java/control/ControleUsuario.java
+++ src/main/java/control/ControleUsuario.java
@@ -1,11 +1,20 @@
 package control;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import com.google.gson.Gson;
+
+import persistence.UsuarioDao;
+import entity.Usuario;
+
+@WebServlet("/autenticarLogin")
 public class ControleUsuario extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
@@ -14,6 +23,39 @@ public class ControleUsuario extends HttpServlet {
 	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			String user = request.getParameter("usuario");
+			String password = request.getParameter("senha");
+			Usuario u = new Usuario(null,user,password,null);
+			
+			u = new UsuarioDao().efetuarLogin(user, password);
+			if(u == null){
+				request.setAttribute("msg", "Combinação LOGIN/SENHA incorreto !");
+				response.getWriter().print("ERROU");
+				request.getRequestDispatcher("index.jsp").forward(request, response);
+				
+			}else if(("administrador").equalsIgnoreCase(u.getPerfil()) ){
+				HttpSession sessao = request.getSession(true);  
+				sessao.setAttribute("usuario",u.getLogin());
+				sessao.setAttribute("idusuario",u.getIdUsuario());
+				//request.getRequestDispatcher("adm/admprincipal.jsp").forward(request, response);
+				String usuarioAutenticado = new Gson().toJson(u);
+				response.setContentType("application/json");
+			    response.setCharacterEncoding("UTF-8");
+			    response.getWriter().write(usuarioAutenticado);
+			}else if(("usuario").equalsIgnoreCase(u.getPerfil()) ){
+				HttpSession sessao = request.getSession(true);  
+				sessao.setAttribute("usuario",u.getLogin());
+				sessao.setAttribute("idusuario",u.getIdUsuario());
+				request.getRequestDispatcher("usu/acadusu.jsp").forward(request, response);
+			}else{
+				request.setAttribute("msg","Ação Inválida");
+				request.getRequestDispatcher("index.jsp").forward(request, response);
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
 	}
 
 }
--- src/main/java/persistence/UsuarioDao.java
+++ src/main/java/persistence/UsuarioDao.java
@@ -13,35 +13,20 @@ public class UsuarioDao extends Dao{
 		rs = stmt.executeQuery();
 		
 		if(rs.next()){
-			u = new Usuario(rs.getInt("idusuario"), rs.getString("login"), rs.getString("senha"), rs.getString("perfil"));
+			u = new Usuario(rs.getInt("idusuario"), rs.getString("login"), null,rs.getString("perfil"));
 		}		
 		close();
 		return u;
 	}
 	
-	/**
-	 * @throws Exception
-	 */
-	public void inserirUsuario() throws Exception{
-		Usuario u = new Usuario(null,"dcewq","felps123","adm");
-		open();		
-		stmt = con.prepareStatement("INSERT INTO USUARIO VALUES(null, ?, ?, ?)");
-		stmt.setString(1, u.getLogin());
-		stmt.setString(2, u.getSenha());
-		stmt.setString(3, u.getPerfil());
-		stmt.executeUpdate();		
-		close();				
-	}
+
 	
 	
 	public static void main(String[] args) {
 		try {
 		//	System.out.println(new UsuarioDao().efetuarLogin("isabela", "isabela123"));
-		 //   new UsuarioDao().inserirUsuario();
-			System.out.println("okqqwe");
-			String felipe = "felps";
-			Integer o = 10;
-			System.out.println(o+10);
+		//   new UsuarioDao().inserirUsuario();
+		
 		} catch (Exception e) {
 			System.out.println(e.getMessage() + "21334");
 		}
